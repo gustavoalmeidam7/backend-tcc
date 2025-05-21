8b2e639bd7044c0b4a22a3cf02aa3be188cb4f7f
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2025-05-20 21:51:27 -0300

    FIX/ Transformado repository em singleton

--- src/repository/UserRepository.py
+++ src/repository/UserRepository.py
@@ -1,6 +1,15 @@
 from src.model.User import User
 
 class UserRepository:
+    instance = None
+    
+    def __init__(self):
+        if not instance:
+            instance = UserRepository()
+        
+        return instance
+
+
     def create(self, userModel: User) -> User:
         userModel.save(force_insert=True)
         return userModel
