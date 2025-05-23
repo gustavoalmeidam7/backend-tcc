381747077e3be08d52d2425c47255ceafddff6b4
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2025-05-23 11:37:38 -0300

    FEAT/ Trocada rota getall para getusers com paginacao

--- src/controller/UserController.py
+++ src/controller/UserController.py
@@ -1,4 +1,4 @@
-from flask import Response, jsonify, request
+from flask import Response, jsonify, request, make_response
 from flask_restx import Resource, Namespace
 
 from src.dto.User.UserCreateDTO import UserDTO
@@ -10,7 +10,7 @@ from src.service.UserService import UserService
 
 import json
 
-api = Namespace("Users", description="Users blueprint", path="/api/user")
+api = Namespace("Users", description="Users blueprint", path="/user")
 
 userService = UserService()
 
@@ -28,8 +28,20 @@ class CreateUser(Resource):
         if result.status == UserStatus.INVALID:
             return Response(json.dumps({"erros": result.message}), 400)
 
-@api.route("/getall")
-class GetAllUsers(Resource):
+@api.route("/getusers")
+class GetUsers(Resource):
     @api.response(200, "Return all registered users", UserResponseDTO.doc_model(api))
+    @api.response(400, "When recive error parsing parameters")
+    @api.param("page", "Page number")
+    @api.param("pagesize", "Page size (between 1 and 50)")
     def get(self):
-        return jsonify(userService.find_all_dict())
+        page = request.args.get("page")
+        pagesize = request.args.get("pagesize")
+
+        if not page.isnumeric():
+            return make_response({"error": "Error parsing page param!"}, 400)
+        
+        if not pagesize.isnumeric():
+            return make_response({"error": "Error parsing pagesize param!"}, 400)
+
+        return make_response(userService.find_all_page_dict(int(page), int(pagesize)), 200)
