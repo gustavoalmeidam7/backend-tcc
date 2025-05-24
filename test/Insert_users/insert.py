fff048628860f5b185c93cafa87728f9f228ebc6
gustavoalmeidam7 <gustavoalmeidam7@gmail.com>
2025-05-24 18:03:28 -0300

    FIX/ Melhorada performace e consistencia do script de testes

--- test/Insert_users/insert.py
+++ test/Insert_users/insert.py
@@ -3,27 +3,26 @@ import requests
 
 URL = "http://localhost:5000/api/user/create"
 
-def create_people(number: int = 1) -> 'list[dict]':
-    listUsers = []
-    for i in range(number):
-        person = people()
+def format_date(date: str) -> str:
+    date = date.split("/")
+    return f"{date[0]}-{date[1]}-{date[2]}"
 
-        nascimento = person[0]["data_nasc"].split("/")
-        nascimento = f"{nascimento[0]}-{nascimento[1]}-{nascimento[2]}"
+def create_users(number: int = 1) -> 'list[dict]':
+    listUsers = []
+    list_person = people(n=number)
+    for person in list_person:
         listUsers.append({
-            "username": person[0]["nome"],
-            "cpf": person[0]["cpf"],
-            "birthday": nascimento,
-            "email": person[0]["email"],
-            "phone_number": person[0]["celular"],
-            "password": person[0]["senha"]
+            "username": person["nome"],
+            "cpf": person["cpf"],
+            "birthday": format_date(person["data_nasc"]),
+            "email": person["email"],
+            "phone_number": person["celular"],
+            "password": person["senha"]
         })
 
     return listUsers
 
-def insert_users(number: int = 1) -> str :
-    users = create_people(number)
-
+def insert_users(users: 'dict' = None) -> str :
     users_created = []
     for user in users:
         content = requests.post(
@@ -39,4 +38,4 @@ def insert_users(number: int = 1) -> str :
 
 nUsers = input("How much users will be generated: ")
 
-print(insert_users(int(nUsers)))
+print(insert_users(create_users(int(nUsers))))
